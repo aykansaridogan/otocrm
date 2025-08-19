e69aa1eedbcb494d63055bf511670ade6569563d
aykansaridogan <aykansaridogan@hotmail.com>
2025-08-19 14:33:49 +0300

    api değişim

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -17,7 +17,7 @@ export function Login() {
     setLoading(true);
 
     try {
-      const response = await fetch('https://7019f606b1fa.ngrok-free.app/api/login', {
+      const response = await fetch('https://4a987081ad79.ngrok-free.app/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
